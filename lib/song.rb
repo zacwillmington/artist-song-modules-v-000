a167945acc1f7ecc936f186b8931d9b308122551
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 14:58:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,17 +21,9 @@ class Song
     @artist = artist
   end
 
-  # def self.reset_all
-  #   @@songs.clear
-  # end
-
   def to_param
     name.downcase.gsub(' ', '-')
   end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def self.all
     @@songs
