cd2584ac6eeea6c2bb1b66b21d14b93d47274783
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 15:08:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,10 +11,6 @@ class Artist
 
   @@artists = []
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def initialize
     @@artists << self
     @songs = []
